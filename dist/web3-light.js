949e7c77f292191671ce38ad467394d933d73ee7
Dmitry Novotochinov <dmitry.novot@gmail.com>
2019-04-09 21:22:35 +0300

    add personal_keycardSignPinless

--- dist/web3-light.js
+++ dist/web3-light.js
@@ -1,4 +1,4 @@
-require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports=[
   {
     "constant": true,
@@ -5630,6 +5630,13 @@ var methods = function () {
 		inputFormatter: [null, formatters.inputAddressFormatter, null]
     });
 
+    var keycardSignPinless = new Method({
+        name: 'keycardSignPinless',
+		call: 'personal_keycardSignPinless',
+		params: 3,
+		inputFormatter: [null, formatters.inputAddressFormatter, null]
+    });
+
     var ecRecover = new Method({
         name: 'ecRecover',
 		call: 'personal_ecRecover',
@@ -5663,6 +5670,7 @@ var methods = function () {
         unlockAccount,
         ecRecover,
         sign,
+        keycardSignPinless,
         sendTransaction,
         lockAccount
     ];
